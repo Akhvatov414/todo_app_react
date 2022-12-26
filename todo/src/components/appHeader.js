c14dac015e8b97845655af04accf940169593836
Akhvatov414 <superbus414@gmail.com>
2022-12-26 16:02:37 +0300

    minor changes
    Убрана структура из обучающего видео.
    Добавлена структура приложения из таска.
    Добавлен макет верстки приложения, дальнейшего распределения по компонентам

--- todo/src/components/appHeader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AppHeader = () => {
-    return (
-        <h1>To do справочник</h1>
-    );
-};
-
-export default AppHeader;
--- todo/src/components/appListItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AppListItem = () => {
-    return (
-        <span>Test</span>
-    );
-};
-
-export default AppListItem;
