5ec0639c337bef33b9b752466073d1093c1e9789
Akhvatov414 <superbus414@gmail.com>
2022-12-27 11:15:11 +0300

    minor changes

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import NewTaskForm from './components/newTaskForm';
+import TaskList from './components/taskList';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('todoapp'));
 root.render(
   <React.StrictMode>
+    <NewTaskForm/>
+    <TaskList/>
   </React.StrictMode>
 );
